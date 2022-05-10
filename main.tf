afca0b8de6597e36ea2490a4ebc8009573b26d47
Samara Ferreira <ferreirasamara.sf@gmail.com>
2022-05-10 16:38:22 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sqs_queue" "queue" {
+  name = "s3-event-notification-queue"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "poc-aws-s3-notification-sqs-bucket"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# poc-aws-s3-notification-sqs
+
+### Event Notification
+
+![alt text](./images/event.png)
+
+
+### notification object
+```json
+{
+  "Service":"Amazon S3",
+  "Event":"s3:TestEvent",
+  "Time":"2022-05-10T19:23:56.905Z",
+  "Bucket":"poc-aws-s3-notification-sqs-bucket",
+  "RequestId":"","HostId":""
+}
+```
+
